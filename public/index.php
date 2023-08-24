6e7f01d8f217bbad31d0f04242d1fed2070089d2
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-24 15:28:53 +0200

    create routes.php

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ require base_path('vendor/autolaod.php');
 define('ENV_FILE', require base_path('env.local.ini'));
 
 $router = new Router;
+require base_path('routes.php');
 
-//pour la relier au controller frontale on va faire une class Router qui va s'occuper des routes
+//On va faire un tableau qui va prendre en lui toutes les routes possibles en fonction de l'uri qu'on va recevoir et de la methode qui a lancé la requete.
+
+//On va aussi associé un controller à cette uri, qui elle va appeler une méthode qui va nous afficher la vue
 
---
+++ routes.php
@@ -0,0 +1,7 @@
+<?php
+$router->get('/', [HomeController::class,'index']);
+
+//Voici notre liste de route possible
+//ici la seule route que nous avonse et '/'
+//et on va la lié une methode index de la HomeController.
+//ça sera une classe qui gere toutes les routes et methodes lié avec la homeController.
