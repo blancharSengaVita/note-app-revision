42a146d1b724fa96519e4be7b8908fb23e78255a
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-24 09:48:14 +0200

    setup env.file.ini

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ define('BASE_PATH', __DIR__ . '/../');
 require BASE_PATH . 'core/functions.php';
 
 require base_path('vendor/autolaod.php');
-
+define('ENV_FILE', require base_path('env.local.ini'));
 
 //La variable globale __DIR__ contient le chemin absolu du répertoire du fichier dans lequel elle est utilisée. Cela signifie que si vous utilisez __DIR__ dans un script PHP, elle vous donnera le chemin complet du répertoire où se trouve ce script
 
