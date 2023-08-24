8afbb8ad34c07defde3b34b9c700d054a3c5fef9
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-24 15:13:23 +0200

    create class Router

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ require BASE_PATH . 'core/functions.php';
 require base_path('vendor/autolaod.php');
 define('ENV_FILE', require base_path('env.local.ini'));
 
+$router = new Router;
 
-
+//pour la relier au controller frontale on va faire une class Router qui va s'occuper des routes
 
