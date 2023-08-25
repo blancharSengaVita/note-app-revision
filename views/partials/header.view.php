3a1dc624e8f8eced49c3e091f6ac298626715e11
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2023-08-25 11:03:09 +0200

    add some style

--- views/partials/header.view.php
+++ views/partials/header.view.php
@@ -1,5 +1,41 @@
 <!doctype html>
 <html lang="fr">
+<style>
+	body{
+		font-family: Mukta;
+	}
+	a{
+		color: black;
+	}
+
+	a:hover{
+		font-weight: 900;
+	}
+
+	a:visited {
+		color: black;
+	}
+
+
+	ul{
+		margin: 0;
+	}
+
+	h2{
+		margin-top: 0;
+	}
+	li{
+		list-style-type: none
+	}
+	body {
+		background-color: antiquewhite;
+	}
+
+	header{
+		background-color: skyblue;
+		width: 100%;
+	}
+</style>
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
@@ -7,3 +43,14 @@
 	<title><?= 'document' ?></title>
 </head>
 <body>
+<header>
+	<nav>
+		<h2 class="sro">
+
+		</h2>
+		<ul style="display: flex">
+			<li style="margin-right:2rem" ><a href="/">accueil</a></li>
+			<li><a href="/notes">Voir les notes</a></li>
+		</ul>
+	</nav>
+</header>
